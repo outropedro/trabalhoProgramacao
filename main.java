2190c1183309126b2af2e97bb4aab5d1010a56fa
outropedro <pedroordep5000@gmail.com>
2020-12-16 09:46:05 -0300

    main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[] args) {
+        //CHAMAR OS METODOS DAS QUESTÕES DENTRO DA MAIN, E CRIAR AS FUNÇÕES SEPARAS. EX: roboVigia();
+
+
+        
+    }
+}
