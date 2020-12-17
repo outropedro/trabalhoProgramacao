3dd551a9a6da9d4b297d6af6aa986ad45b377435
outropedro <pedroordep5000@gmail.com>
2020-12-17 10:39:06 -0300

    17/12/2020

--- main.java
+++ main.java
@@ -4,9 +4,8 @@ public class main{
     public static void main(String[] args) {
         //CHAMAR OS METODOS DAS QUESTÕES DENTRO DA MAIN, E CRIAR AS FUNÇÕES SEPARAS. EX: roboVigia();
 
-        roboVigia();
+        //roboVigia();
         smurfsDaSegundona();
-
 
     }
 
@@ -18,7 +17,6 @@ public class main{
     public static void smurfsDaSegundona() {
         int tamanhoChuteira, qtdChuteiras,aux,contE, contD;
         String l;
-        String direito="Direito", esquerdo="Esquedro";
  
 
         Scanner teclado = new Scanner(System.in);
@@ -34,28 +32,58 @@ public class main{
             aux = 1+i;
             System.out.print("Insira o número da " + aux + "ª chuteira: ");
             tamanhoChuteira = teclado.nextInt();
-            System.out.print("Qual dos pés ela pertence?\nD - Direito\nE - Esquerdo:  ");
-            l = teclado.nextLine();
-            //l = teclado.next().charAt(0); //so para teste
+            System.out.print("Essa chuteira é de qual pé:\nD - Direito\nE - Esquerdo: ");
             teclado.nextLine();
+            l=teclado.nextLine();
 
-            //Não quer entrar no IF
 
-            if(l.equals("D")){
-                System.out.print("teste");
-            }
-            else{
-               
-            }
-
+            if(tamanhoChuteira == tamanhoChuteira){
+                //conta quantas chuteiras tem de cada pé
+                 if(l.equals("d") || l.equals("D")){
+                    contD++;
+                }
+                else if(l.equals("E") || l.equals("e")){
+                    contE++;
+                }
             
+       
+        }   
         }
 
-
-        //System.out.print(contE +" "+contD);
+        if(contD==contE){
+            System.out.print(contD);
+        }
+        else if(contD<contE){
+            System.out.print(contD);
+        }
+        else if(contE<contD){
+            System.out.print(contE);
+        }
     }
 
+    public static void semRepeticao(){
+        int tamVetor, min, max,aux =0, cont=0;
 
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Insira o numero minimo desejado: ");
+        min = teclado.nextInt();
+
+        System.out.print("Insira o numero maximo desejado: ");
+        max = teclado.nextInt();
+
+        tamVetor = max-min;
+        int vetor[] = new int[tamVetor];
+        
+
+        for (int i=0; i<tamVetor; i++){
+            //IMPLEMENTAR UMA FORMA QUE O VETOR RECEBA NUMERO POR NUMERO DESMEBRADO E EXECUTE A COMPARAÇÃO SE TEM ALGUM IGUAL, DEPOIS ZERAR VETOR, DEPOIS COLOCAR IF 
+            // E ELSE PARA CONTAR QUANTAS NUMEROS SEM OS DIGITOS REPETIDOS TEM DENTRO DO INTERVALO
+        }
+        
+
+
+    }
 
 
 
